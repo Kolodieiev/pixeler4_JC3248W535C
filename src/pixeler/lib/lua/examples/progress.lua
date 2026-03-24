ce996093e2c4de259c11ca2f51998f101e2360e0
Пройдисвіт <92428846+Kolodieiev@users.noreply.github.com>
2026-03-24 21:11:11 +0200

    Додано приклади луа

---
+++ src/pixeler/lib/lua/examples/progress.lua
@@ -0,0 +1,31 @@
+initType("IWidgetContainer")
+initType("ProgressBar")
+
+main_layout = context.getLayout() 
+main_layout:setBackColor(COLOR_GREY) 
+
+progress_bar = ProgressBar:new(1)
+main_layout:addWidget(progress_bar)
+progress_bar:setWidth(gl:width() - 80)
+progress_bar:setHeight(20)
+progress_bar:setPos((gl.width() - progress_bar:getWidth()) * 0.5, (gl.height() - progress_bar:getHeight()) * 0.5) 
+progress_bar:setMax(100)
+progress_bar:setBackColor(COLOR_BLACK)
+progress_bar:setProgressColor(COLOR_GREEN)
+progress_bar:setBorderColor(COLOR_ORANGE)
+progress_bar:setBorder(true)
+
+function update()
+    local swipe = input.getSwipe()
+    if swipe == SWIPE_L then
+		context.exit()
+        return;
+	end
+
+    local progress = progress_bar:getProgress()
+    if progress < 100 then
+        progress_bar:setProgress(progress + 1)
+    else 
+        progress_bar:reset()
+    end
+end
